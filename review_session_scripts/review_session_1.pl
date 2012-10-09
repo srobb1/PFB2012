ab0abaf6c80feeadb7fd2246cf68e2f78a06f327
Sofia Robb <sofia2341@gmail.com>
2012-10-09 08:19:42 -0700

    new review script

---
+++ review_session_scripts/review_session_1.pl
@@ -0,0 +1,6 @@
+#!/usr/bin/perl -w
+use strict;
+
+my $scalar = 'this is a scalar';
+my @array = ("this" , "is", "an" , "array");
+my %hash = ( "word_1" => "this" , "word_2" => "is" , "word_3" => "a" , "word_4" => "hash");
