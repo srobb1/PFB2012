1839c52dcdd168469c69dd1d87187067b6bd307d
davemessina <online@davemessina.com>
2012-10-17 16:30:25 -0400

    adding Perl II scripts

---
+++ lecture_scripts/Perl_II_Operators_Control_structures/ifnot.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+my $ok;
+
+if (not $ok) {
+    print "not ok\n" 
+}
+
+
---
+++ lecture_scripts/Perl_II_Operators_Control_structures/ifnot2.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+my $ok = 100;
+
+if ($ok < 100 and $ok > 0) {
+    print "it's smaller than 100!\n";
+}
